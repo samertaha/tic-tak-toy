53911559f9e4d787daef2e578533372cab790ad9
Samer Taha <alrazy1@gmail.com>
2021-10-04 18:46:30 +0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,50 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import "./index.css";
+
+const Square = () => {
+  return <div className='square'>x</div>;
+};
+
+const Board = () => {
+  return (
+    <div
+      style={{
+        backgroundColor: "skyblue",
+        margin: 10,
+        padding: 20,
+      }}
+    >
+      Board
+      <div className='board-row'>
+        <Square />
+        <Square />
+        <Square />
+      </div>
+      <div className='board-row'>
+        <Square />
+        <Square />
+        <Square />
+      </div>
+      <div className='board-row'>
+        <Square />
+        <Square />
+        <Square />
+      </div>
+    </div>
+  );
+};
+
+const Game = () => {
+  return (
+    <div className='game'>
+      Game
+      <Board />
+    </div>
+  );
+};
+
+ReactDOM.render(<Game />, document.getElementById("root"));
+
+// great change
